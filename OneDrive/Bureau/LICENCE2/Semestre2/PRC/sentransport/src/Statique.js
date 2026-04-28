8da5855478cffc0233846219c56c388acd8e1011
Ndeye Diegui Diedhiou <diedhioun4@gmail.com>
2026-04-28 16:38:52 +0000

    Lab 2 : composants statiques LigneBus et ListeLignes

---
+++ OneDrive/Bureau/LICENCE2/Semestre2/PRC/sentransport/src/Statique.js
@@ -0,0 +1,12 @@
+import './Statistique.css';
+
+function Statistique() {
+    return (
+        <div className="statistique">
+            <span className="stat-nombre">10</span>
+            <span className="stat-label">lignes</span>
+        </div>
+    );
+}
+
+export default Statistique;
